2f5cff3136ebc6eb965d954ea0904fc90d799a5f
maelgalland <maelgalland.71@gmail.com>
2022-10-14 11:38:27 +0200

    js

--- JS/login.js
+++ JS/login.js
@@ -1,12 +1,62 @@
-const slidePage=document.querySelector('.slide-page')
-const firsNext=document.querySelector('.next-1')
+const slidePage=document.querySelector('.slide-page');
+const firstNext=document.querySelector('.next-1');
+const secNext=document.querySelector('.next-2');
+const secondPrevious=document.querySelector('.prev-2');
+const thirdPrevious=document.querySelector('.prev-3');
+const thirdNext=document.querySelector('.next-3');
+const fourthPrevious=document.querySelector('.prev-4');
+const fourthNext=document.querySelector('.next-4');
 const circle = document.querySelectorAll(".step .circle");
 const progressText = document.querySelectorAll(".step .name-step");
 const progressCheck = document.querySelectorAll(".step .check");
 let current =1;
-firsNext.addEventListener('click',()=>{
-   circle[current -1].classList.add("active");
+
+firstNext.addEventListener('click',()=>{
+    slidePage.style.marginLeft = "-25%";
+    circle[current -1].classList.add("active");
     progressCheck[current - 1].classList.add("active");
     progressText[current - 1].classList.add("active");
    current+=1;
 })
+
+secNext.addEventListener('click',()=>{
+    slidePage.style.marginLeft = "-50%";
+    circle[current -1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    current+=1;
+})
+
+thirdNext.addEventListener('click',()=>{
+    slidePage.style.marginLeft = "-75%";
+    circle[current -1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    current+=1;
+})
+fourthNext.addEventListener('click',()=>{
+    console.log("Done")
+})
+secondPrevious.addEventListener('click',()=>{
+    slidePage.style.marginLeft = "0%";
+    circle[current - 2].classList.remove("active");
+    progressCheck[current - 2].classList.remove("active");
+    progressText[current - 2].classList.remove("active");
+    current -= 1;
+})
+thirdPrevious.addEventListener('click',()=>{
+    slidePage.style.marginLeft = "-25%";
+    circle[current - 2].classList.remove("active");
+    progressCheck[current - 2].classList.remove("active");
+    progressText[current - 2].classList.remove("active");
+    current -= 1;
+})
+fourthPrevious.addEventListener('click',()=>{
+    slidePage.style.marginLeft = "-50%";
+    circle[current - 2].classList.remove("active");
+    progressCheck[current - 2].classList.remove("active");
+    progressText[current - 2].classList.remove("active");
+    current -= 1;
+})
+
+
