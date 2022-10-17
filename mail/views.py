9307487ce934cd80c6dd35bf6f98794fe0cc1013
Mattéo Miguelez <matteo.miguelez@outlook.com>
2022-10-17 14:22:53 +0200

    commentaires

--- mail/views.py
+++ mail/views.py
@@ -3,6 +3,7 @@ from django.core.mail import send_mail
 import random
 
 def verif_email(request):
+    ''' Fonction qui permet l'envoi d'un mail à un utilisateur depuis l'adresse mail du site web '''
     try:
         send_mail("Vérification de votre compte - Ne pas répondre",
         "Code de vérification :\n"
@@ -16,6 +17,7 @@ def verif_email(request):
     return render(request, 'send/mailtest.html')
 
 def random_value():
+    ''' Fonction qui renvoie une chaîne composée de 6 caractères entre 0 et 9 '''
     value_random = ""
     for i in range(6):
         value_random += str(random.randint(0, 9))
