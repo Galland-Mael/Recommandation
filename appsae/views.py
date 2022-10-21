73ae831daa620d1c30f51058ac866da87971e011
antoine <antoineguillot38@gmail.com>
2022-10-21 17:38:53 +0200

    début html + form

--- appsae/views.py
+++ appsae/views.py
@@ -3,7 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import RestaurantType
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    nom = RestaurantType.objects.order_by('id')[:5]
+    template = loader.get_template('appsae/index.html')
+    context = {
+        'nom': nom,
+    }
+
+    return HttpResponse(template.render(context, request))
