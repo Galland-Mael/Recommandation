e699aa69e1d2e59d4862988cc7271c513756248a
maelgalland <maelgalland.71@gmail.com>
2022-11-03 16:30:53 +0100

    start modif user

--- static/JS/modifUser.js
+++ static/JS/modifUser.js
@@ -1,6 +1,21 @@
-const logo =document.querySelector('.logo-img');
+const profilContainer =document.querySelector('.profil-container');
 const profil = document.querySelector('.profil');
+const changeProfil = document.querySelector('.profil-span')
 
-logo.addEventListener('click',()=>{
+profilContainer.addEventListener('click',()=>{
    profil.click();
 })
+
+profilContainer.addEventListener('mouseover',()=>{
+   changeProfil.classList.remove('opacity-null')
+   changeProfil.classList.add('normal-opacity')
+})
+
+profilContainer.addEventListener('mouseout',()=>{
+   changeProfil.classList.add('opacity-null')
+   changeProfil.classList.remove('normal-opacity')
+})
+
+
+
+
