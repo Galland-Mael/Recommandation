ebdadd71aec5996a2a468d28ad9b90eef25d0041
maelgalland <maelgalland.71@gmail.com>
2022-11-04 01:44:55 +0100

    Create adherent done without picture

--- appsae/models.py
+++ appsae/models.py
@@ -14,6 +14,7 @@ class Adherant(models.Model):
     birthDate = models.DateField("Date", default=datetime.date.today)
     pseudo = models.CharField(max_length=20)
     password =models.CharField(max_length=20)
+    profile_picture = models.ImageField(upload_to='media/img/profile_pictures')
 
 class Groupe(models.Model):
     nom_groupe = models.CharField(max_length=25)
