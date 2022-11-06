b73899d6e0f283d321f499d697c4ab863670c654
maelgalland <maelgalland.71@gmail.com>
2022-11-06 21:56:13 +0100

    pas de verif mail car trop relou

--- appsae/models.py
+++ appsae/models.py
@@ -11,6 +11,9 @@ class Adherant(models.Model):
     password =models.CharField(max_length=20)
     profile_picture = models.ImageField(default='static/Image/avatar.jpeg', upload_to='media/img/profile_pictures',blank=True)
 
+    def __str__(self):
+        return self.mail
+
 class Groupe(models.Model):
     nom_groupe = models.CharField(max_length=25)
 
