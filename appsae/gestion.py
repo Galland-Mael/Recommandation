1a5a91a9dc2fe498025128de493e88645346312c
Mattéo Miguelez <matteo.miguelez@outlook.com>
2022-12-08 14:23:02 +0100

    MAJ de la fonction pour les carrousels

--- appsae/gestion.py
+++ appsae/gestion.py
@@ -4,22 +4,19 @@ from django.db.models import Avg
 NB_CARROUSEL = 10
 
 
-def liste_carrousel(type):
+def listeAffichageCaroussel(type=""):
     """ Renvoie les meilleurs restaurants selon le type de restaurant donné en paramètres,
-    si il n'y pas de filtre, le paramètre d'entrée est la chaine "tous"
+    si il n'y pas de filtre, le paramètre d'entrée est la chaine "", donnée par défaut
 
     @param type: le type de restaurant recherché
-    @type type : str
     @return: les meilleurs restaurants selon le filtre
     """
-    if type != "tous":
+    if type != "":
         type_restaurant = RestaurantType.objects.filter(nom=type.lower())  # on stock le type de restaurant correspondant au filtre
         if type_restaurant.count() == 0: # si il n'y a pas de type avec ce nom on arête la fonction
             return []
-        restaurant = Restaurant.objects.filter(type=type_restaurant[0]).order_by('-note')[:NB_CARROUSEL]
-    else:
-        restaurant = Restaurant.objects.order_by('-note')[:NB_CARROUSEL]
-    return restaurant
+        return Restaurant.objects.filter(type=type_restaurant[0]).order_by('-note')[:NB_CARROUSEL]
+    return Restaurant.objects.order_by('-note')[:NB_CARROUSEL]
 
 
 def update_note_moyenne_restaurant(nomRestaurant):
