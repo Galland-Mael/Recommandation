92ef9cf311795b71b0077fa0e2801aff321ca8d6
Mattéo Miguelez <matteo.miguelez@outlook.com>
2023-02-09 23:35:58 +0100

    models admin

--- appsae/models.py
+++ appsae/models.py
@@ -13,7 +13,15 @@ class Restaurateur(models.Model):
     password = models.CharField(max_length=255)
     profile_picture = models.ImageField(default='img_user/avatar.jpeg', upload_to='img_user/')
 
-    def __str(self):
+    def __str__(self):
+        return self.mail
+
+
+class Administrateur(models.Model):
+    mail = models.EmailField(max_length=254)
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
         return self.mail
 
 
